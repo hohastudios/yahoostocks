dc589cfaf5fa3945d2e61873dd0ec26102fbfb5c
hohastudios <hohastudios@gmail.com>
2024-10-14 00:51:05 +0800

    adding fair value

--- ui.py
+++ ui.py
@@ -8,12 +8,16 @@ st.title("Yahoo Stocks Dashboard")
 sym = st.text_input("Enter stock", "0005.HK")
 yfc = Yfc()
 symInfo=yfc.getInfo(sym)
+# (net profit * 1.022)/0.07/diluted shares
+netprofit=symInfo.income_stmt.loc['Net Income']
+dilutedshares=symInfo.income_stmt.loc['Diluted Average Shares']
+st.write("Fair value based on prev net income")
+st.write(netprofit.iloc[0] * 1.04 / 0.07/dilutedshares.iloc[0])
 
 st.write("Dividends", symInfo.dividends)
 #for key in symInfo.basic_info:
 #    st.write(key,symInfo.basic_info[key])
 
-st.write("Net Profit")
 #for key in symInfo.income_stmt.keys():
 #    st.write(key, symInfo.income_stmt[key]['Net Income'])
 #logger.info(f'{symInfo.income_stmt.keys()}')
